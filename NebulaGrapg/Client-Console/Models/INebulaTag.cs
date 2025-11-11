f7215465bda7bd0761ed0338326313b973e70002
The-Night-Raven <hphilip141@gmail.com>
2025-11-11 21:08:44 +0100

    - implemented insert logic - Added test DDL schema

--- NebulaGrapg/Client-Console/Models/INebulaTag.cs
+++ NebulaGrapg/Client-Console/Models/INebulaTag.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client_Console.Models
+namespace Client.Console.Models
 {
-    public interface INebulaModel
+    public interface INebulaTag
     {
         string ModelName { get; }
-        int Index { get; set; }
+        Guid Index { get; }
         string Create();
 
     }
--- NebulaGrapg/Client-Console/Program.cs
+++ NebulaGrapg/Client-Console/Program.cs
@@ -1,23 +1,29 @@
-﻿using Nebula.Graph;
+﻿using Client.Console.Models;
+using Client.Console.Services;
+using Nebula.Graph;
 using NebulaNet;
 using Thrift.Protocol;
 
-namespace Client_Console
+namespace Client.Console
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            using (NebulaConnection client = new NebulaConnection())
+            using (NebulaService service = new NebulaService("127.0.0.1", 9669, "root", "nebula"))
             {
-                await client.OpenAsync("127.0.0.1", 9669);
-                AuthResponse authResponse = await client.AuthenticateAsync("root", "nebula");
-                ExecutionResponse executionResponse = await client.ExecuteAsync(authResponse.Session_id, "USE Test;");
+                await service.ExecuteAsync("USE UCLTest;");
+                School school = new School("UCL","https://www.ucl.dk/");
+                Location UCLSeebladsgade = new Location("Seebladsgade 1", 5000, "Odense", "UCL Seebladsgade", 55.403450, 10.379370);
+                Location UCLVestreEngvej = new Location("Vestre Engvej 51 C", 7100, "Vejle", "UCL Vestre Engvej", 55.707780, 9.517108);
+                SchoolHasLocation schoolHasUCLSeebladsgade = new SchoolHasLocation(school, UCLSeebladsgade);
+                SchoolHasLocation schoolHasUCLVestreEngvej = new SchoolHasLocation(school, UCLVestreEngvej);
+                await service.CreateVertexAsync(school);
+                await service.CreateVertexAsync(UCLSeebladsgade);
+                await service.CreateEdgeAsync(schoolHasUCLSeebladsgade);
+                ExecutionResponse executionResponse1 = await service.CreateEdgeAsync(schoolHasUCLVestreEngvej);
 
-                await client.ExecuteAsync(authResponse.Session_id, "INSERT VERTEX Schools (Name, Website) VALUES 1:(\"UCL\",\"https://www.ucl.dk/\")");
-                await client.ExecuteAsync(authResponse.Session_id, "INSERT VERTEX Location (InternalName, Location) VALUES 1:(\"Odense\",ST_Point(55.403450,10.379370))");
-                await client.ExecuteAsync(authResponse.Session_id, "INSERT VERTEX Location (InternalName, Location) VALUES 2:(\"Vejle\",ST_Point(55.707396,9.517151))");
-                
+
             }
         }
     }
